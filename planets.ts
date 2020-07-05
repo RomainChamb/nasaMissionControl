e39434b47e4d6093692a39c7887e4c64515f3322
Romain Chambonnier <romain.chambonnier+git@protonmail.com>
2020-07-05 17:17:13 +0200

    adding allowMethods + planets files

---
+++ planets.ts
@@ -0,0 +1,54 @@
+// Standard modules
+import { join } from "https://deno.land/std/path/mod.ts";
+import { BufReader } from "https://deno.land/std/io/bufio.ts";
+import { parse } from "https://deno.land/std/encoding/csv.ts";
+
+// Third party modules
+import * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
+
+interface Planet {
+    [ key : string ] : string
+}
+
+async function loadPlanetsData() {
+    const path = join(".", "kepler_exoplanets_nasa.csv");
+
+    const file = await Deno.open(path);
+    const bufReader = new BufReader(file);
+    const result = await parse(bufReader, { header: true, comment: "#" });
+    Deno.close(file.rid);
+
+    const planets = (result as Array<Planet>).filter((planet) => {
+        const planetaryRadius = Number(planet["koi_prad"]);
+        const stellarMass = Number(planet["koi_smass"]);
+        const stellarRadius = Number(planet["koi_srad"]);
+        return planet["koi_disposition"] === "CONFIRMED"
+        && planetaryRadius > 0.5 && planetaryRadius < 1.5
+        && stellarMass > 0.78 && stellarMass < 1.04
+        && stellarRadius > 0.99 && stellarRadius < 1.01;
+    });
+
+    return planets.map((planet) => {
+        return _.pick(planet, [
+            "koi_prad",
+            "koi_smass",
+            "koi_srad",
+            "kepler_name",
+            "koi_count",
+            "koi_steff",
+            "koi_period"
+        ]);
+    });
+}
+
+const newEarths = await loadPlanetsData();
+
+console.log(`${newEarths.length} habitable planets found !`);
+for (const planet of newEarths) {
+    console.table(planet);
+}
+
+
+const sortedEarth = newEarths.sort((planetA, planetB) => Number(planetA.koi_period) - Number(planetB.koi_period));
+console.log(`${sortedEarth[0].kepler_name} has the shortest orbital period : ${sortedEarth[0].koi_period} days`);
+console.log(`${sortedEarth[sortedEarth.length - 1].kepler_name} has the longest orbital period : ${sortedEarth[sortedEarth.length - 1].koi_period} days`);
