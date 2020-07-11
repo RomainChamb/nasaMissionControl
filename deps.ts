2975226afae160ac64ad339b179bcd780758a492
Romain Chambonnier <romain.chambonnier+git@protonmail.com>
2020-07-11 15:55:53 +0200

    adding test_deps.ts to manage test dependencies

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 // Standard library depencies
-export * as log from "https://deno.land/std/log/mod.ts";
-export { join } from "https://deno.land/std/path/mod.ts";
-export { BufReader } from "https://deno.land/std/io/bufio.ts";
-export { parse } from "https://deno.land/std/encoding/csv.ts";
+export * as log from "https://deno.land/std@0.60.0/log/mod.ts";
+export { join } from "https://deno.land/std@0.60.0/path/mod.ts";
+export { BufReader } from "https://deno.land/std@0.60.0/io/bufio.ts";
+export { parse } from "https://deno.land/std@0.60.0/encoding/csv.ts";
 
 // Third party depencies
 export { Application, send, Router } from "https://deno.land/x/oak@v5.3.1/mod.ts";
